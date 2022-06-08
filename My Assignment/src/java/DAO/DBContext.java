15224058a57f1ec15132f573f0b7e241a703f6a5
thanhnthe1 <91208887+thanhnthe1@users.noreply.github.com>
2022-06-09 02:07:45 +0700

    Connect DBContext
    Connect DbContext

---
+++ My Assignment/src/java/DAO/DBContext.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ngo Tung Son
+ */
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "thanhnt";
+            String pass = "12345678";
+            String url = "jdbc:sqlserver://localhost\\DESKTOP-DT03IGN:1433;databaseName=ATDatabase";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
+    
+}
